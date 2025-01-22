553fdf5bb1bcab1e534de04e325d50b1be3843dd
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2025-01-22 11:16:00 +0530

    brute force

--- 1-TwoSum.py
+++ 1-TwoSum.py
@@ -10,14 +10,21 @@
 #         return lst1
 
 
+# class Solution:
+#     def twoSum(self, nums: List[int], target: int) -> List[int]:
+#         # lst1 = []
+#         for i in range(len(nums)):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i]+nums[j] == target:
+#                     # lst1.append(i)
+#                     # lst1.append(j)
+#                     return [i,j]
+#         # return lst1
+#         return []
+
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        # lst1 = []
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i]+nums[j] == target:
-                    # lst1.append(i)
-                    # lst1.append(j)
-                    return [i,j]
-        # return lst1
-        return []
+        for index1 in range(len(nums)):
+            for index2 in range(index1+1, len(nums)):
+                if nums[index1]+nums[index2] == target:
+                    return [index1, index2]
