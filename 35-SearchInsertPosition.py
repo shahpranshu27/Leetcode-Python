c5fe30df7bfc0eb3c4ee3cfa6d5cb6b5b5a9244f
Pranshu shah <97401387+shahpranshu27@users.noreply.github.com>
2025-01-22 18:51:49 +0530

    Update 35-SearchInsertPosition.py

--- 35-SearchInsertPosition.py
+++ 35-SearchInsertPosition.py
@@ -1,11 +1,11 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        for i in range(len(nums)):
-            if nums[i] == target:
-                return i
+        for index in range(len(nums)):
+            if nums[index] == target:
+                return index
 
-            elif target<nums[i]:
-                return i
+            elif target<nums[index]:
+                return index
             
         return len(nums)
             
